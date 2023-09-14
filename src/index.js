c2bf6ec035989507eb9a4737f83ce499f0fc69e3
MaidedPonce <maided.h.p90@gmail.com>
2023-09-13 22:12:46 -0600

    An observer was added!

--- src/index.js
+++ src/index.js
@@ -4,27 +4,40 @@ import CarouselItem from './CarouselItem'
 
 const SectionTitle = title => h('h3.carousel-title', title)
 
-const Carousel = ({ itemsList = [] }) =>
-  h(
-    'section.carousel',
-    h(
-      'div',
-      itemsList.map(
-        ({
-          attributes: { titles, posterImage, slug, youtubeVideoId, startDate },
-        }) =>
-          CarouselItem({
-            imageUrl: posterImage.large,
-            title: titles.en,
-            subtitle: titles.ja_jp,
-            slug,
-            youtubeVideoId,
-            startDate,
-          })
-      )
-    )
+const Carousel = ({ itemsList = [] }) => {
+  const carouselItems = itemsList.map(
+    ({
+      attributes: { titles, posterImage, slug, youtubeVideoId, startDate },
+    }) =>
+      CarouselItem({
+        imageUrl: posterImage.medium,
+        title: titles.en,
+        subtitle: titles.ja_jp,
+        slug,
+        youtubeVideoId,
+        startDate,
+      })
   )
 
+  const options = {
+    rootMargin: '10px',
+    threshold: 0.2,
+  }
+  const imagesObserver = new IntersectionObserver(elements => {
+    elements.forEach(entry => {
+      const image = entry.target.querySelector('img')
+      if (entry.isIntersecting) {
+        image.style.display = 'block'
+        imagesObserver.unobserve(image)
+      } else {
+        image.style.display = 'none'
+      }
+    })
+  }, options)
+  carouselItems.forEach(img => imagesObserver.observe(img))
+
+  return h('section.carousel', h('div', carouselItems))
+}
 window.addEventListener('DOMContentLoaded', async () => {
   const mountReference = document.querySelector('.main').lastElementChild
 
