22282d6051a92a7d51b4b4f16129981d433d070c
MaidedPonce <maided.h.p90@gmail.com>
2023-09-15 22:54:21 -0600

    Analizer was added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
 const webpack = require('webpack')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 const path = require('path')
+const shouldAnalize = process.argv.includes('--analyze')
+const plugins = []
+
+if (shouldAnalize) {
+  plugins.push(new BundleAnalyzerPlugin())
+}
 
 const nodeEnv = process.env.NODE_ENV || 'development'
 
@@ -9,10 +17,16 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist',
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
   },
   devServer: {
     contentBase: '.',
+  },
+  plugins,
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 }
 
