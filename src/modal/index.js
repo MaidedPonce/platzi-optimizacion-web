8a447053507c5a44a585ca0ec1736823c86bca15
MaidedPonce <maided.h.p90@gmail.com>
2023-09-22 18:06:04 -0600

    Adding new dependencies

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,7 +1,17 @@
+// import { openModal } from "./open-modal";
+
 export const modalListener = event => {
   event.preventDefault()
   const img = event.target
   const link = img.parentElement
+  const videoId = link.dataset.videoid
 
+  if (link && link.classList.contains('js-link-link')) {
+    import(/* webpackChunkName: "modal" */ './open-modal').then(
+      ({ openModal }) => {
+        openModal(videoId)
+      }
+    )
+  }
   console.log(link.href)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist',
+    publicPath: '/dist/',
     filename: '[name].bundle.js',
   },
   devServer: {
@@ -27,6 +27,14 @@ const config = {
     splitChunks: {
       chunks: 'all',
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 }
 
